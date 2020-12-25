bf99e745bc34a6438fe1f08ce93a71715bb72aa2
syahriikram <syahriikram@gmail.com>
2020-12-25 16:14:22 +0800

    Fixed app.cs

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import Chat from './Chat';
 
 function App() {
@@ -8,16 +7,6 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Chat />
-=======
-import Main from './components/Main';
-
-function App() {
-  return (
-    <div className="app">
-      <div className="app__body">
-        <Main />
-
->>>>>>> efef82e407e292193a648a210c59cd501526338a
       </div>
     </div>
   );
